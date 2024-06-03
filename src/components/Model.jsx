9bcb21b64999204a547b1dcb609db20e51e91c56
Nahuel Romero - Syn <106286182+NahuelRomeroZADA@users.noreply.github.com>
2024-06-03 01:20:39 -0300

    model 3d v1

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -3,6 +3,7 @@ import gsap from "gsap";
 import ModelView from "./ModelView";
 import { useRef, useState } from "react";
 import { yellowImg } from "../utils";
+import{ models, sizes } from "../constants";
 
 import * as THREE from 'three';
 import { Canvas } from "@react-three/fiber";
@@ -78,12 +79,24 @@ const Model = () => {
                         <View.Port/>
                     </Canvas>
                 </div>
+            <div className="mx-auto w-full">
+                <p className="text-sm font-light text-center mb-5">{model.title}</p>
 
+                    <div className="flex-center">
+                        <ul className="color-container">
+                            {models.map((item, i) => (
+                                <li key={i} 
+                                className="w-6 h-6 rounded-full mx-2 cursor-pointer" 
+                                style={{ backgroundColor: item.color[0] }} 
+                                onClick={() => setModel(item)} />
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </div>
-        
+        </div> 
     </section>
-  )
+)
 }
 
 export default Model
